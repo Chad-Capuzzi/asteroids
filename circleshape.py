66141dd0691291acbb0f8639557fa3d186b0a6f5
Chad-Capuzzi <capuzzichad12@gmail.com>
2025-07-20 13:07:00 -0400

    asteroid collision causes game over

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def check_collision(self, other):
+        # checks if player ship collides with an asteroid
+        return self.position.distance_to(other.position) <= (self.radius + other.radius)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 
 def main():
@@ -29,6 +30,10 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.check_collision(asteroid):
+                sys.exit("Game over!")
 
         screen.fill("black")
 
